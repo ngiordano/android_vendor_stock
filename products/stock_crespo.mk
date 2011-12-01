f994d345e21c94b67d24c93dace79617f3244838
codenamedroid <codenamegit@gmail.com>
2011-12-01 10:18:45 -0600

    add build identifiers and rom manager compatibility

--- products/stock_crespo.mk
+++ products/stock_crespo.mk
@@ -6,7 +6,10 @@ $(call inherit-product, vendor/stock/config/common_full_phone.mk)
 # Inherit some common stuff.
 $(call inherit-product, vendor/stock/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_ID=GWK74 BUILD_DISPLAY_ID=ITL41D BUILD_FINGERPRINT=google/soju/crespo:2.3.7/GWK74/185293:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.7 GWK74 185293 release-keys" BUILD_NUMBER=185293
+# Release name
+PRODUCT_RELEASE_NAME := NS
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_ID=GWK74 BUILD_FINGERPRINT=google/soju/crespo:2.3.7/GWK74/185293:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.7 GWK74 185293 release-keys" BUILD_NUMBER=185293
 
 PRODUCT_NAME := stock_crespo
 PRODUCT_DEVICE := crespo
--- products/stock_maguro.mk
+++ products/stock_maguro.mk
@@ -6,7 +6,10 @@ $(call inherit-product, vendor/stock/config/common_full_phone.mk)
 # Inherit some common stuff.
 $(call inherit-product, vendor/stock/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_ID=ITL41D BUILD_DISPLAY_ID=ITL41D BUILD_FINGERPRINT="google/yakju/maguro:4.0.1/ITL41D/223971:user/release-keys" PRVIATE_BUILD_DESC="yakju-user 4.0.1 ITL41D 223971 release-keys"
+# Release name
+PRODUCT_RELEASE_NAME := GN
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_ID=ITL41D  BUILD_FINGERPRINT="google/yakju/maguro:4.0.1/ITL41D/223971:user/release-keys" PRVIATE_BUILD_DESC="yakju-user 4.0.1 ITL41D 223971 release-keys"
 
 PRODUCT_NAME := stock_maguro
 PRODUCT_DEVICE := maguro
