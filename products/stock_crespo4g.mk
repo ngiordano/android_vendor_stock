0bf9c36fc63a04260e2b0ce8297c74d94ba2669e
codenamedroid <codenamegit@gmail.com>
2011-12-18 03:31:06 -0600

    fix up nexus s 4g config

--- products/stock_crespo4g.mk
+++ products/stock_crespo4g.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/stock/config/common_full_phone.mk)
 # Release name
 PRODUCT_RELEASE_NAME := NS4G
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_ID=IML74K BUILD_FINGERPRINT=google/soju/crespo4g:4.0.3/IML74K/185293:user/release-keys PRIVATE_BUILD_DESC="soju-user 4.0.3 IML74K 185293 release-keys" BUILD_NUMBER=185293
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=sojus BUILD_ID=IML74K BUILD_FINGERPRINT=google/sojus/crespo4g:4.0.3/IML74K/239410:user/release-keys PRIVATE_BUILD_DESC="sojus-user 4.0.3 IML74K 239410 release-keys" BUILD_NUMBER=239410
 
 PRODUCT_NAME := stock_crespo4g
 PRODUCT_DEVICE := crespo4g
