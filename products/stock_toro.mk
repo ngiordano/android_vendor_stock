aff57bb9565fa162c21209b19cea436fb04ada0a
codenamedroid <codenamegit@gmail.com>
2011-12-28 14:33:41 -0600

    shorten maguro and toro release names

--- products/stock_toro.mk
+++ products/stock_toro.mk
@@ -10,7 +10,7 @@ PRODUCT_COPY_FILES +=  \
     vendor/cnd/prebuilt/common/xhdpi/bootanimation.zip:system/media/bootanimation.zip
 
 # Release name
-PRODUCT_RELEASE_NAME := GN(CDMA/LTE)
+PRODUCT_RELEASE_NAME := GN-CDMA
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=yakju BUILD_ID=IML74K  BUILD_FINGERPRINT="google/yakju/toro:4.0.3/IML74K/223971:user/release-keys" PRVIATE_BUILD_DESC="yakju-user 4.0.3 IML74K 223971 release-keys"
 
