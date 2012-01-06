3328b3c6f462690a3f011db918411920d2f4b35e
codenamedroid <codenamegit@gmail.com>
2012-01-06 15:14:09 -0600

    a few extra overrides and fix sounds

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -3,13 +3,14 @@ $(call inherit-product, vendor/stock/config/common.mk)
 
 # Bring in all audio files
 include frameworks/base/data/sounds/AllAudio.mk
-include frameworks/base/data/sounds/AudioPackage7.mk
 
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Girtab.ogg \
     ro.config.notification_sound=Proxima.ogg \
-    ro.config.alarm_alert=Cesium.ogg
+    ro.config.alarm_alert=Cesium.ogg \
+    ro.kernel.android.checkjni=0 \
+    windowsmgr.max_events_per_sec=240
 
 PRODUCT_PACKAGES += \
   Mms
